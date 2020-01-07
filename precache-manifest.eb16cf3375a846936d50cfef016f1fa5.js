8f550e9c11c65956f27ac39b80a2a00e5649ecfa
revortni <rojit.blaze13@gmail.com>
2020-01-08 00:32:02 +0545

    Updates

---
+++ precache-manifest.eb16cf3375a846936d50cfef016f1fa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e60913d9487d7846daf4e12a2a8aac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "745b3cdf22fe43678fa9",
+    "url": "/static/css/main.545724bb.chunk.css"
+  },
+  {
+    "revision": "2831a3995b69003fdaa9",
+    "url": "/static/js/2.b59aacd1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b59aacd1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "745b3cdf22fe43678fa9",
+    "url": "/static/js/main.37da5503.chunk.js"
+  },
+  {
+    "revision": "e1bf6edfcf2325271041",
+    "url": "/static/js/runtime-main.990e2d2c.js"
+  }
+]);
